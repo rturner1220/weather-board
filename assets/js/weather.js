317e490f258e93c74805546eec86f827159bde7a
Rosa Turner <rmturner1220@gmail.com>
2022-06-04 13:02:45 -0400

    addind api weather

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,14 +1,18 @@
+// add functionality to pull current location weather
+// create button to clear the city array
+var cityList = [];
+var id = "f1a31b169738f6caeabf63c37fa3d56b";
 
+// stores citiList in localStorage
+function storeCities() {
+    localStorage.setItem("cities", JSON.stringify(cityList));
+}
 
-
-var cityFormE1 = document.querySelector("#city-form");
-var nameInputE1 = document.querySelector("#cityname");
-
-// form submission browser event
-var fromSubmitHandler = function(event) {
-    event.preventDefoult();
-    console.log(event);
-};
-
-// add submit even listener
-cityFormE1.addEventListener("search", fromSubmitHandler);
+// adds last searched city to list-group as button for user to select city
+function createCityList() {
+    $(".cityList").empty();
+    cityList.forEach(function(city) {
+        $(".cityList").prepend($(`<button class="list-group-item list-group-item-action cityButton"
+        data-city="${city}">${city}</button>`));
+    })
+}
